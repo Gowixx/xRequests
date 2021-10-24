64eec0935f97035c5cd2a3c150c3a13904c0d367
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-10-24 13:24:36 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='xrequests',
+    author='h0nda',
+    version=0.1,
+    url='https://github.com/Gowixx/xRequests',
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    install_requires=['pysocks', 'brotli']
+)
