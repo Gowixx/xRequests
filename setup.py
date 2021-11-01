89569662f23206a4cf821a7f58f63a5d5b9c82d7
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-11-01 13:53:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name='xrequests',
-    author='h0nda',
-    version=0.1,
+    author='h0nda + Gowixx',
+    version=0.2,
     url='https://github.com/Gowixx/xRequests',
     packages=setuptools.find_packages(),
     classifiers=[],
