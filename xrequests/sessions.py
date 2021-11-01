aa62a00204576f1d6788e7ede5319e0ac4e5afae
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-11-01 10:43:07 -0500

    Update sessions.py

--- xrequests/sessions.py
+++ xrequests/sessions.py
@@ -2,41 +2,27 @@ from .exceptions import *
 from .structures import CaseInsensitiveDict
 from .models import Response
 from urllib.parse import urlsplit
-import brotli
-import gzip
-import socks
-import socket
-import ssl
-import zlib
+import brotli, gzip, socks, socket, ssl, zlib
 
 scheme_to_proxy_type = {
-    "http": socks.HTTP,
-    "https": socks.HTTP,
-    "socks": socks.SOCKS4,
-    "socks5": socks.SOCKS5,
-    "socks5h": socks.SOCKS5
+    'http': socks.HTTP, 'https': socks.HTTP,
+    'socks': socks.SOCKS4, 'socks4': socks.SOCKS4,
+    'socks5': socks.SOCKS5, 'socks5h': socks.SOCKS5
 }
 
 scheme_to_port = {
-    "http": 80,
-    "https": 443
+    'http': 80,
+    'https': 443,
+    'socks': 1080, 'socks4': 1080, 'socks5': 1080
 }
 
 class Session:
-    def __init__(self, proxies=None, timeout=None, chunk_size=None,
-                 decode_content=None, verify=None, ciphers=None):
-        proxies = proxies if proxies is not None else {}
-        timeout = timeout if timeout is not None else 60
-        chunk_size = chunk_size if chunk_size is not None else (1024 ** 2)
-        decode_content = decode_content if decode_content is not None else True
-        verify = verify if verify is not None else True
-
+    def __init__(self, proxies={}, timeout=60, chunk_size=(1024 ** 2), decode_content=True, verify=True, ciphers=None):
         if proxies:
             for scheme, proxy_url in proxies.items():
                 proxy = urlsplit(proxy_url)
                 if scheme not in scheme_to_port:
-                    raise UnsupportedScheme(
-                        f"'{scheme}' is not a supported scheme")
+                    raise UnsupportedScheme("'{}' is not a supported scheme".format(scheme))
                 if proxy.scheme not in scheme_to_proxy_type:
                     raise UnsupportedScheme(
                         f"'{proxy.scheme}' is not a supported proxy scheme")
@@ -71,7 +57,7 @@ class Session:
             self._addr_to_conn.pop(addr, None)
 
     def request(self, method, url, headers=None, data=None, timeout=None,
-                version=None, verify=None, ciphers=None):
+                version='1.1', verify=None, ciphers=None):
         ciphers = ciphers if ciphers is not None else self.ciphers
         parsed_url = urlsplit(url)
 
@@ -79,23 +65,19 @@ class Session:
             raise UnsupportedScheme(
                 f"'{parsed_url.scheme}' is not a supported scheme")
         
-        if verify is None:
-            verify = self.verify
-
-        if version is None:
-            version = "1.1"
+        if not verify: verify = self.verify
 
         if not isinstance(headers, CaseInsensitiveDict):
             headers = CaseInsensitiveDict(headers)
 
-        if not "Host" in headers:
+        if not 'Host' in headers:
             headers["Host"] = parsed_url.hostname
 
-        if data is not None:
+        if data:
             if not isinstance(data, bytes):
                 data = data.encode("utf-8")
 
-            if "Content-Length" not in headers:
+            if 'Content-Length' not in headers:
                 headers["Content-Length"] = int(len(data))
 
         host_addr = (
@@ -106,8 +88,7 @@ class Session:
 
         request = self._prepare_request(
             method=method,
-            path=(parsed_url.path \
-                + ("?" + parsed_url.query if parsed_url.query else "")) or "/",
+            path=('{}?{}'.format(parsed_url.path, parsed_url.query) or '') or "/",
             version=version,
             headers=headers,
             body=data
@@ -125,7 +106,7 @@ class Session:
                         proxy=self._scheme_to_proxy.get(parsed_url.scheme),
                         timeout=timeout if timeout is not None \
                                 else self.timeout,
-                        ssl_wrap=("https" == parsed_url.scheme),
+                        ssl_wrap=('https' == parsed_url.scheme),
                         ssl_verify=verify,
                         ciphers=ciphers)
                     self._addr_to_conn[host_addr] = conn
@@ -145,30 +126,7 @@ class Session:
 
                 conn_reused = False
 
-    def get(self, url, **kwargs):
-        return self.request("GET", url, **kwargs)
-
-    def post(self, url, **kwargs):
-        return self.request("POST", url, **kwargs)
-
-    def options(self, url, **kwargs):
-        return self.request("OPTIONS", url, **kwargs)
-
-    def head(self, url, **kwargs):
-        return self.request("HEAD", url, **kwargs)
-
-    def put(self, url, **kwargs):
-        return self.request("PUT", url, **kwargs)
-
-    def patch(self, url, **kwargs):
-        return self.request("PATCH", url, **kwargs)
-
-    def delete(self, url, **kwargs):
-        return self.request("DELETE", url, **kwargs)
-
-    def _create_socket(self, dest_addr, proxy=None, timeout=None,
-                       ssl_wrap=True, ssl_verify=True, remote_dns=False,
-                       ciphers=None):
+    def _create_socket(self, dest_addr, proxy=None, timeout=None, ssl_wrap=True, ssl_verify=True, remote_dns=False, ciphers=None):
         if proxy is None:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
@@ -200,18 +158,16 @@ class Session:
 
     @staticmethod
     def _prepare_request(method, path, version, headers, body):
-        request = f"{method} {path} HTTP/{version}\r\n"
+        request = "{} {} HTTP/{}\r\n".format(method, path, version)
 
         for header, value in headers.items():
             if value is None:
                 continue
-            request += f"{header}: {value}\r\n"
+            request += "{}: {}\r\n".format(header, value)
+        request += '\r\n'
+        request = request.encode('UTF-8')
 
-        request += "\r\n"
-        request = request.encode("UTF-8")
-
-        if body is not None:
-            request += body
+        if body: request += body
 
         return request
 
@@ -220,7 +176,7 @@ class Session:
         resp = conn.recv(max_chunk_size)
 
         if not resp:
-            raise EmptyResponse("Empty response from server")
+            raise EmptyResponse('Server returned an empty response.')
 
         while not b"\r\n\r\n" in resp:
             resp += conn.recv(max_chunk_size)
@@ -233,7 +189,7 @@ class Session:
         headers = CaseInsensitiveDict()
         for header in raw_headers.splitlines():
             header, value = header.split(":", 1)
-            value = value.lstrip(" ")
+            value = value.lstrip(' ')
             if header in headers:
                 if isinstance(headers[header], str):
                     headers[header] = [headers[header]]
@@ -243,7 +199,7 @@ class Session:
         
         # download chunks until content-length is met
         if get_content:
-            if "content-length" in headers:
+            if 'content-length' in headers:
                 goal = int(headers["content-length"])
                 while goal > len(data):
                     chunk = conn.recv(min(goal-len(data), max_chunk_size))
@@ -290,7 +246,6 @@ class Session:
         elif encoding == "deflate":
             content = zlib.decompress(content)
         else:
-            raise UnsupportedEncoding(
-                f"Unknown encoding type '{encoding}' while decoding content")
-        
+            raise UnsupportedEncoding('{} is not a valid encoding.'.format(encoding))
+
         return content
